ddcb4a9e34d47680d203fa426d476748d2a94f15
Dmitriy Belozerov <dm.belozerov@yandex.ru>
2024-04-12 13:35:01 +0300

    feat: js component 2 level 2

--- js/task-js/component-2/js/script.js
+++ js/task-js/component-2/js/script.js
@@ -1,7 +1,12 @@
+
 (function () {
   // /* Код компонента пишите здесь */
   const phone = document.querySelector('#phone');
   const regex = /(?:\+)[\d\-\(\)\ ]{9,}\d/g;
+
+  const inputDayStart = document.querySelector('#checkin-date');
+  const inputDayEnd = document.querySelector('#checkout-date');
+
 
   const btn = document.querySelector('.submit-btn');
   btn.addEventListener('click', (e) => {
@@ -10,12 +15,26 @@
     if (regex.test(phoneValue)) {
       phone.classList.add('field-correct');
       phone.classList.remove('field-error');
-
     } else {
       phone.classList.add('field-error');
       phone.classList.remove('field-correct');
     }
 
+    const dateStart = new Date(inputDayStart.value);
+    const dateEnd = new Date(inputDayEnd.value);
+    const dateDiffDays = (dateEnd - dateStart) / 1000 / 3600 / 24;
+
+    if (dateDiffDays > 3) {
+      inputDayStart.classList.add('field-correct');
+      inputDayStart.classList.remove('field-error');
+      inputDayEnd.classList.add('field-correct');
+      inputDayEnd.classList.remove('field-error');
+    } else {
+      inputDayStart.classList.add('field-error');
+      inputDayStart.classList.remove('field-correct');
+      inputDayEnd.classList.add('field-error');
+      inputDayEnd.classList.remove('field-correct');
+    }
   });
 
 
