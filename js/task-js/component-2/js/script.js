c420aee85ea61c425b9c393be37a2fbd236dc99f
Dmitriy Belozerov <dm.belozerov@yandex.ru>
2024-04-12 12:31:40 +0300

    feat: js component 2 level 1

--- js/task-js/component-2/js/script.js
+++ js/task-js/component-2/js/script.js
@@ -1,5 +1,25 @@
-(function() {
-  /* Код компонента пишите здесь */
+(function () {
+  // /* Код компонента пишите здесь */
+  const phone = document.querySelector('#phone');
+  const regex = /(?:\+)[\d\-\(\)\ ]{9,}\d/g;
+
+  const btn = document.querySelector('.submit-btn');
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const phoneValue = phone.value;
+    if (regex.test(phoneValue)) {
+      phone.classList.add('field-correct');
+      phone.classList.remove('field-error');
+
+    } else {
+      phone.classList.add('field-error');
+      phone.classList.remove('field-correct');
+    }
+
+  });
+
+
+
 
 
 })();
